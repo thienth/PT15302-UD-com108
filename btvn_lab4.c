efdee0825cf79337cf03e4696350e7415c535099
thienth32 <thienth32@gmail.com>
2019-10-03 11:10:21 +0700

    Create btvn_lab4.c

---
+++ btvn_lab4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    // yêu cầu nhập 3 số bất kỳ không âm
+    
+    // 1. Kiểm tra xem 3 số có hình thành được tam giác
+    // hay không - nếu không thì yêu cầu nhập lại
+    // 2. tìm số lớn nhất trong 3 số
+    // 3. Tính chu vi, diện tích của tam giác
+    return 0;
+}
